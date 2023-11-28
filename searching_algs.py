a95c989f32a53d3363cdca5493d52d39c66e5ecc
Ellen C <ellenchapman999@gmail.com>
2023-11-28 16:47:26 +0000

    recursive binary search

--- searching_algs.py
+++ searching_algs.py
@@ -39,3 +39,24 @@ result = binary_search(numbers, 15)
 verify(result)
 result = binary_search(numbers, 5)
 verify(result)
+
+# Recursive Binary search
+
+def recursive_binary_search(list, target):
+	if len(list) == 0:
+		return False
+	else:
+		midpoint = (len(list)//2)
+		if list[midpoint] == target:
+			return True
+		else:
+			if list[midpoint] < target:
+				return recursive_binary_search(list[midpoint+1:], target)
+			else:
+				return recursive_binary_search(list[:midpoint], target)
+
+def verify2(result):
+	print("Target found: ", result)
+
+result = recursive_binary_search(numbers, 2)
+verify2(result)
