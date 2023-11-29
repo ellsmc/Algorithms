6aea480fff058b4bb2e1115f45d52a7f10e9b819
Ellen C <ellenchapman999@gmail.com>
2023-11-29 13:45:16 +0000

    Merge sort

--- sorting_algs.py
+++ sorting_algs.py
@@ -23,3 +23,74 @@ def bubbleSort(array):
         unsortedTilIndex -= 1
     # When a comparrison runs through without a swap (passthrough) then return sorted array
     return array
+#_____________________________________________________________________________________________________________________________________________________
+# MERGE SORT
+#------------------
+def merge_sort(list):
+    """
+    Sort into ascending order
+    Out: new sorted list
+    Divide to find midpt and sublists
+    Recursively sort sublists
+    Merge sorted sublists
+    O(kn log n)
+    """
+    # Naively sorted conditions
+    if len(list) <= 1:
+        return list
+    
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(list):
+    """
+    Divide unsorted list at midpt into sublists
+    Return sublists (left, right)
+    splitting is constant time k
+    O(k log n) time
+    """
+    mid = len(list)//2
+    left = list[:mid]
+    right = list[mid:]
+
+    return left, right
+
+def merge(left, right):
+    """
+    Merges two lists(arrays), sorts them
+    Returns new merged list
+    O(n)
+    """
+    l = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1
+    while i < len(left):
+        l.append(left[i])
+        i+=1
+    while j < len(right):
+        l.append(right[j])
+        j+=1
+    return l
+
+def verify(list):
+    n = len(list)
+    if n == 0 or n == 1:
+        return True
+    return list[0] < list[1] and verify(list[1:])
+
+sortme = [ 43, 7, 3, 222, 9, 14, 12, 6]
+l = merge_sort(sortme)
+# print(l)
+print(verify(sortme))
+print(verify(l))
