1e08396801c579ca86affbbf7de4c4c4f79dde6b
Ellen C <ellenchapman999@gmail.com>
2023-11-29 13:18:16 +0000

    added functionality to linked list

--- lists.py
+++ lists.py
@@ -34,6 +34,11 @@ python -i lists.py
 >>> l3.add(3)
 >>> l3
 [Head: 3]-> [2]-> [Tail: 1]
+>>> l.add(45)
+>>> l.search(45)
+<Node data: 45>
+>>> l
+[Head: 45]-> [3]-> [2]-> [Tail: 1]
 """
 
 
@@ -86,6 +91,64 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def search(self, key):
+        """
+        search for FIRST node which matches key
+        Linear time O(n)
+        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Insert new node at index position
+        Ins - O(1), search - O(n): Overall O(n)
+        """
+        if index == 0:
+            self.add(data)
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+            while position > 1:
+                current = current.next_node
+                position -= 1
+            prev_node = current
+            next_node = current.next_node
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove_at_index():
+        pass
+
+    def remove_at_value(self, key):
+        """
+        Removes node containing key and returns it
+        O(n) time
+        """
+        current = self.head
+        previous = None
+        found = False
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+        return current
+
+    def value_at_index():
+        pass
 
     def __repr__(self):
         """
