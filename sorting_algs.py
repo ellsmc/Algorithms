a179b9542614018e19f7336475f2ff04cbad61fe
Ellen C <ellenchapman999@gmail.com>
2023-11-30 20:53:31 +0000

    Selection sort

--- sorting_algs.py
+++ sorting_algs.py
@@ -186,3 +186,38 @@ LL.add(123)
 print(LL)
 sorted_LL = merge_sort_LL(LL)
 print(sorted_LL)
+
+
+#______________________________________________________________________________________________________________________________________________________
+# SELECTION SORT
+#------------------
+import sys
+from load import load_numbers
+
+numbers = load_numbers(sys.argv[1])
+# numbers = [4, 68, 90, 43, 3 ,7, 8, 9]
+
+def index_of_min(values):
+    min_index = 0
+    for i in range(1, len(values)):
+        if values[i] < values[min_index]:
+            min_index = i
+    return min_index
+
+def selection_sort(values):
+    sorted_list = []
+    # print("%-25s %-25s" % (values, sorted_list))
+    for i in range(0, len(values)):
+        index_to_move = index_of_min(values)
+        sorted_list.append(values.pop(index_to_move))
+        # print("%-25s %-25s" % (values, sorted_list))
+    return sorted_list
+
+print(selection_sort(numbers))
+
+
+
+
+#______________________________________________________________________________________________________________________________________________________
+#  SORT
+#------------------
