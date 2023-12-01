7f82b168264b84c372f9e2db4e9e30c02b064eb9
Ellen C <ellenchapman999@gmail.com>
2023-12-01 12:01:16 +0000

    Tidying + Linear search w/ strings

--- searching_algs.py
+++ searching_algs.py
@@ -1,5 +1,8 @@
-# Linear search
+#______________________________________________________________________________________________________________________________________________________
+# LINEAR SEARCH
 # O(n) complexity (Linear)
+#------------------
+
 def linear_search(list, target):
 	"""
 	Returns index of target. Else returns none - could also raise exception?
@@ -20,8 +23,37 @@ verify(result)
 result = linear_search(numbers, 8)
 verify(result)
 
-# Binary search
+
+###############################
+# LINEAR SEARCH - STRINGS
+#  
+#------------------
+import sys
+from load import load_strings
+
+# names = load_strings(sys.argv[1])
+names = ["Lucie Hansman", "Mariann Melena",  "Sibyl Froeschle", "Ray Spratling"]
+search_names = ["Francina Vigneault", "Lucie Hansman", "Nancie Rubalcaba", "Elida Sleight", "Guy Lashbaugh", "Ginger Schlossman", "Suellen Luaces", "Jamey Kirchgesler", "Amiee Elwer", "Lacresha Peet", "Leonia Goretti", "Carina Bunge", "Renee Brendeland", "Andrew Mcgibney", "Gina Degon", "Deandra Pihl", "Desmond Steindorf", "Magda Growney", "Tawana Srivastava", "Tammi Todman", "Harley Mussell", "Iola Bordenet", "Edwardo Khela", "Myles Deanne", "Elden Dohrman", "Ira Hooghkirk", "Eileen Stigers", "Mariann Melena", "Maryrose Badura", "Ardelia Koffler", "Lacresha Kempker", "Charlyn Singley", "Lekisha Tawney", "Christena Botras", "Mike Blanchet", "Cathryn Hinkson", "Errol Shinkle", "Mavis Bhardwaj", "Sung Filipi", "Keiko Dedeke", "Lorelei Morrical", "Jimmie Lessin", "Adrianne Hercules", "Latrisha Haen", "Denny Friedeck", "Emmett Whitesell", "Sina Sauby", "Melony Engwer", "Alina Reichel", "Rosamond Shawe", "Elinore Benyard", "Sang Bouy", "Ed Aparo", "Sheri Wedding", "Sang Snellgrove", "Shaquana Sones", "Elvia Motamed", "Candice Lucey", "Sibyl Froeschle", "Ray Spratling", "Cody Mandeville", "Donita Cheatham", "Darren Later", "Johnnie Stivanson", "Enola Kohli", "Leann Muccia", "Carey Philps", "Suellen Tohonnie", "Evelynn Delucia", "Luz Kliment", "Lettie Jirjis", "Francene Klebe", "Margart Scholz", "Sarah Growden", "Glennis Gines", "Rachael Ojima", "Teofila Stample", "Narcisa Shanley", "Gene Lesnick", "Malena Applebaum", "Norma Tingey", "Marianela Mcmullen", "Rosalva Dosreis", "Dallas Heinzmann", "Sade Streitnatter", "Lea Pelzel", "Judith Zwahlen", "Hope Vacarro", "Annette Ayudan", "Irvin Cyree", "Scottie Levenstein", "Agustina Kobel", "Kira Moala", "Fawn Englar", "Jamal Gillians", "Karen Lauterborn", "Kit Karratti", "Steven Deras", "Mary Rosenberger", "Alonso Viviano"]
+
+def index_of_item(collection, target):
+	for i in range(0, len(collection)):
+		if target == collection[i]:
+			return i
+	return None
+
+for n in search_names:
+	index = index_of_item(names, n)
+	print(index)
+
+
+
+
+
+#______________________________________________________________________________________________________________________________________________________
+# BINARY SEARCH
 # O(log n) complexity (Logarithmic), constant space
+#------------------
+
 def binary_search(list, target):
 	first = 0
 	last = len(list) - 1
@@ -40,8 +72,11 @@ verify(result)
 result = binary_search(numbers, 5)
 verify(result)
 
-# Recursive Binary search
+###############################
+# BINARY SEARCH - RECURSIVE
 #  Time O(log n)
+#------------------
+
 def recursive_binary_search(list, target):
 	if len(list) == 0:
 		return False
